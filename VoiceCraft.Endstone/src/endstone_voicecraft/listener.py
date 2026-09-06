cd0294b1ba0dca868047604c1cd5c066e9985b40
SamSo...? <samsosleepy@gmail.com>
2026-09-06 23:28:59 +0700

    Add Endstone join and quit listeners

---
+++ VoiceCraft.Endstone/src/endstone_voicecraft/listener.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from endstone.event import PlayerJoinEvent, PlayerQuitEvent, event_handler
+
+
+class VoiceCraftListener:
+    def __init__(self, plugin) -> None:
+        self._plugin = plugin
+
+    @event_handler
+    def on_player_join(self, event: PlayerJoinEvent) -> None:
+        self._plugin.handle_player_join(event.player)
+
+    @event_handler
+    def on_player_quit(self, event: PlayerQuitEvent) -> None:
+        self._plugin.handle_player_quit(event.player)
