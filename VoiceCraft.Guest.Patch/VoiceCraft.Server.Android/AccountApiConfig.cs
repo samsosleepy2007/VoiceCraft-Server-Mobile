e4ecb9965fd84c37dda4bdd5b268c193a13696e2
SamSo...? <samsosleepy@gmail.com>
2026-09-12 00:40:36 +0700

    stage Guest Account API config

---
+++ VoiceCraft.Guest.Patch/VoiceCraft.Server.Android/AccountApiConfig.cs
@@ -0,0 +1,13 @@
+namespace VoiceCraft.Server.Android;
+
+internal static class AccountApiConfig
+{
+    public const string AccountApiBase =
+        "https://azzmvholzjucmzhqitwa.supabase.co/functions/v1/voicecraft-account";
+
+    public const string GuestApiBase =
+        "https://azzmvholzjucmzhqitwa.supabase.co/functions/v1/voicecraft-guest";
+
+    public const string AppVersion =
+        "1.7.1-android-phase2-ui4.5-account-v2-guest";
+}
