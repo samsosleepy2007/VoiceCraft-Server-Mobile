3b3501f040f1e3db8034aff863e678f2d01a64c6
SamSo...? <samsosleepy@gmail.com>
2026-09-11 21:31:41 +0700

    feat(account): add Supabase project config

---
+++ VoiceCraft.Server.Android/SupabaseConfig.cs
@@ -0,0 +1,7 @@
+namespace VoiceCraft.Server.Android;
+
+internal static class SupabaseConfig
+{
+    internal const string ProjectUrl = "https://azzmvholzjucmzhqitwa.supabase.co";
+    internal const string PublishableKey = "sb_publishable_lXehhsFdrTEPEDlrGuvVbg_TYsAqIM5";
+}
