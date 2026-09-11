f39488de9b63a63a7594656a421e64de1ba2e051
SamSo...? <samsosleepy@gmail.com>
2026-09-12 04:36:00 +0700

    route Android sign-in through isolated mobile account API

--- VoiceCraft.Guest.Patch/VoiceCraft.Server.Android/AccountApiConfig.cs
+++ VoiceCraft.Guest.Patch/VoiceCraft.Server.Android/AccountApiConfig.cs
@@ -3,11 +3,11 @@ namespace VoiceCraft.Server.Android;
 internal static class AccountApiConfig
 {
     public const string AccountApiBase =
-        "https://azzmvholzjucmzhqitwa.supabase.co/functions/v1/voicecraft-account";
+        "https://azzmvholzjucmzhqitwa.supabase.co/functions/v1/voicecraft-mobile";
 
     public const string GuestApiBase =
         "https://azzmvholzjucmzhqitwa.supabase.co/functions/v1/voicecraft-guest";
 
     public const string AppVersion =
-        "1.7.1-android-phase2-ui4.5-account-v2-guest";
+        "1.7.1-android-phase2-ui4.5.2-account-v2-guest";
 }
