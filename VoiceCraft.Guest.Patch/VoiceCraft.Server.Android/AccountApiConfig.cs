02aeb6b956ca6f8ceceb3ee73e0027a7596cc74b
SamSo...? <samsosleepy@gmail.com>
2026-09-13 02:41:26 +0700

    Update mobile API app version for release

--- VoiceCraft.Guest.Patch/VoiceCraft.Server.Android/AccountApiConfig.cs
+++ VoiceCraft.Guest.Patch/VoiceCraft.Server.Android/AccountApiConfig.cs
@@ -9,5 +9,5 @@ internal static class AccountApiConfig
         "https://azzmvholzjucmzhqitwa.supabase.co/functions/v1/voicecraft-guest";
 
     public const string AppVersion =
-        "1.7.1-android-phase2-ui4.5.2-account-v2-guest";
+        "1.7.1-android-phase2-ui4.5.3-account-v2-guest";
 }
